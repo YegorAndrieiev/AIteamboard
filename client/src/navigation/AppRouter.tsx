a79df4ebc9b5723ea8f067e56bfe59030e989a13
Andrieiev1 <132195597+Andrieiev1@users.noreply.github.com>
2026-05-18 11:34:42 +0300

    feat: add docker compose with frontend, backend and postgres

---
+++ client/src/navigation/AppRouter.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../pages/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage';
+import { ResetPage } from '../pages/ResetPage';
+import { TasksPage } from '../pages/TaskPage';
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset" element={<ResetPage />} />
+        <Route path="/tasks" element={<TasksPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
